362bb0306cf9c36f8f9a568fdcfb0988a9acb995
Jeremy Williamson <jeremy.williamson83@gmail.com>
2021-12-21 00:30:35 -0500

    got basic functionality in

--- source/main.go
+++ source/main.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -27,36 +30,94 @@ func main() {
 
 	file.Close()
 
-	// for _, eachline := range txtlines {
-	// 	fmt.Println(eachline)
-	// }
 	run := true
+	board := getBoard()
 	for run {
-		run = execute(txtlines)
+		run = runBoggle(board, txtlines)
 	}
 	fmt.Print("exiting gracefully")
 }
 
-func CheckInput(input string, txtLines []string) bool {
-
-	return true
-	// for _, line := range txtLines {
-
-	// }
-}
-
-func execute(txtlines []string) bool {
-
-	// var input string
-
-	for _, txtline := range txtlines {
-		fmt.Println(txtline)
-		// fmt.Println("enter a word:")
-		// fmt.Scanln(&input)
-		var cont = "y"
-		if !CheckInput(cont, txtlines) {
-			break
+func listContains(match string, wordList []string) bool {
+	for _, txtline := range wordList {
+		stringSections := strings.Split(txtline, "/")
+		word := stringSections[0]
+		if word == match {
+			return true
 		}
 	}
 	return false
+}
+
+func getBoard() [4][4]string {
+	//taken from: https://boardgames.stackexchange.com/questions/29264/boggle-what-is-the-dice-configuration-for-boggle-in-various-languages
+	availableDice := [][]string{
+		// get letters
+		{"I ", "F ", "E ", "H ", "E ", "Y "},
+		{"R ", "I ", "F ", "O ", "B ", "X "},
+		{"U ", "T ", "O ", "K ", "N ", "D "},
+		{"D ", "E ", "N ", "O ", "W ", "S "},
+		{"H ", "M ", "S ", "R ", "A ", "O "},
+		{"L ", "U ", "P ", "E ", "T ", "S "},
+		{"Y ", "L ", "G ", "K ", "U ", "E "},
+		{"QU", "B ", "M ", "J ", "O ", "A "},
+		{"E ", "H ", "I ", "S ", "P ", "N "},
+		{"V ", "E ", "T ", "I ", "G ", "N "},
+		{"B ", "A ", "L ", "I ", "Y ", "T "},
+		{"E ", "Z ", "A ", "V ", "N ", "D "},
+		{"R ", "A ", "L ", "E ", "S ", "C "},
+		{"U ", "W ", "I ", "L ", "R ", "G "},
+		{"P ", "A ", "C ", "E ", "M ", "D "},
+		{"A ", "C ", "I ", "T ", "O ", "A "},
+	}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(availableDice), func(i, j int) { availableDice[i], availableDice[j] = availableDice[j], availableDice[i] })
+
+	var board [4][4]string
+
+	for i, column := range board {
+		for j, _ := range column {
+			board[i][j], availableDice = getRandomLetter(availableDice)
+		}
+	}
+	return board
+
+}
+
+func getRandomLetter(allDice [][]string) (string, [][]string) {
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(len(allDice))
+	j := rand.Intn(6)
+	returnString := allDice[i][j]
+	allDice[i] = allDice[len(allDice)-1]
+	return returnString, allDice[:len(allDice)-1]
+}
+
+func printBoard(board [4][4]string) {
+	for i, column := range board {
+		for j, _ := range column {
+			fmt.Print(board[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+func runBoggle(board [4][4]string, txtlines []string) bool {
+
+	var input string
+
+	printBoard(board)
+
+	fmt.Println("Enter word to check:")
+	fmt.Scanln(&input)
+
+	if listContains(input, txtlines) {
+		fmt.Println("Got it! ＼(＾O＾)／")
+	} else {
+		fmt.Println("Not in the list ¯\\_(ツ)_/¯")
+	}
+
+	fmt.Println("again, y/n?")
+	fmt.Scanln(&input)
+	return input[0] == 'y'
 }
