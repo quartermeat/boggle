5e5405cce1776f7cff890fd2f61500943e880208
Jeremy Williamson <jeremy.williamson83@gmail.com>
2021-12-20 21:53:20 -0500

    got list in and user input

--- source/main.go
+++ source/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Print("hello world")
+	//open file
+	file, err := os.Open("resources/en_US.dic")
+
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	//create scanner from file
+	scanner := bufio.NewScanner(file)
+	//split by scan lines
+	scanner.Split(bufio.ScanLines)
+
+	var txtlines []string
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	file.Close()
+
+	// for _, eachline := range txtlines {
+	// 	fmt.Println(eachline)
+	// }
+	run := true
+	for run {
+		run = execute(txtlines)
+	}
+	fmt.Print("exiting gracefully")
+}
+
+func CheckInput(input string, txtLines []string) bool {
+
+	return true
+	// for _, line := range txtLines {
+
+	// }
+}
+
+func execute(txtlines []string) bool {
+
+	// var input string
+
+	for _, txtline := range txtlines {
+		fmt.Println(txtline)
+		// fmt.Println("enter a word:")
+		// fmt.Scanln(&input)
+		var cont = "y"
+		if !CheckInput(cont, txtlines) {
+			break
+		}
+	}
+	return false
 }
